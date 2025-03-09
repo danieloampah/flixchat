65ff0746573c8bde652670fcad4433ebbc0cb4e2
DESMOND <desmondadnelson@gmail.com>
2025-03-09 12:03:25 +0000

    main

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+<<<<<<< HEAD
 # flixchat
 FlixChat App
+=======
+# FlixChat
+
+A new Flutter project.
+
+## Getting Started
+
+FlutterFlow projects are built to run on the Flutter _stable_ release.
+>>>>>>> 932f92e (rf)
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'FlixChat',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+    );
+  }
+}
